4f663ee077503ca51199cec532cb62c44db7a837
Divakar30r <150545013+Divakar30r@users.noreply.github.com>
2025-08-08 02:33:28 +0530

    Delete ReactSBoot/ReactSBootMongo directory

--- ReactSBoot/ReactSBootMongo/backend/src/main/java/com/example/app/repository/RecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.app.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.app.model.Record;
-
-public interface RecordRepository extends MongoRepository<Record, String> {
-}
