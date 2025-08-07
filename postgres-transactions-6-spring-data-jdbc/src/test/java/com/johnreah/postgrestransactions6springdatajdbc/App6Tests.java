c3ff4eeefe2018639625495aaae49e511fe3dcf7
Divakar30r <150545013+Divakar30r@users.noreply.github.com>
2025-08-08 02:41:36 +0530

    pull request from  your branchJava sources to main (#11)
    Attempt 1

---
+++ postgres-transactions-6-spring-data-jdbc/src/test/java/com/johnreah/postgrestransactions6springdatajdbc/App6Tests.java
@@ -0,0 +1,59 @@
+package com.johnreah.postgrestransactions6springdatajdbc;
+
+import com.johnreah.postgrestransactions6springdatajdbc.entities.Customer;
+import com.johnreah.postgrestransactions6springdatajdbc.repositories.CustomerRepository;
+import com.johnreah.postgrestransactions6springdatajdbc.support.AbstractIntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class App6Tests extends AbstractIntegrationTest {
+
+	Logger log = LoggerFactory.getLogger("com.johnreah.postgrestransactions6springdatajdbc.App6Tests");
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@BeforeEach
+	public void beforeEach() {
+
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testContainerRuns() throws InterruptedException {
+		assertTrue(postgreSQLContainer.isRunning());
+	}
+
+	@Test
+	public void testCustomersExist() {
+		List<Customer> customerList = customerRepository.findByIdNotNull();
+		assertTrue(customerList.size() > 0, "Num customers should be > 0");
+	}
+
+	@Test
+	public void whenNewCustomerSaved_thenSavedOk() {
+		Customer customer = Customer.builder()
+				.firstName("firstName")
+				.lastName("lastName")
+				.email("email")
+				.reference("reference")
+				.build();
+		long numBefore = customerRepository.count();
+		log.debug("Count before = " + numBefore);
+		customerRepository.save(customer);
+		long numAfter = customerRepository.count();
+		log.debug("Count after = " + numAfter);
+		assertTrue(numAfter == numBefore + 1, "Num customers should have incremented by one");
+	}
+}
---
+++ postgres-transactions-6-spring-data-jdbc/src/test/java/com/johnreah/postgrestransactions6springdatajdbc/support/DatabaseUtils.java
@@ -0,0 +1,87 @@
+package com.johnreah.postgrestransactions6springdatajdbc.support;
+
+import com.johnreah.postgrestransactions6springdatajdbc.entities.Account;
+import com.johnreah.postgrestransactions6springdatajdbc.entities.AccountHistory;
+import com.johnreah.postgrestransactions6springdatajdbc.entities.AccountType;
+import com.johnreah.postgrestransactions6springdatajdbc.entities.Customer;
+import com.johnreah.postgrestransactions6springdatajdbc.repositories.AccountRepository;
+import com.johnreah.postgrestransactions6springdatajdbc.repositories.AccountTypeRepository;
+import com.johnreah.postgrestransactions6springdatajdbc.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.stereotype.Component;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Component
+public class DatabaseUtils {
+
+    @Autowired
+    private DatabaseUtilsHelperRepository databaseUtilsHelperRepository;
+
+    @Autowired
+    private AccountTypeRepository accountTypeRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public void deleteEverything() {
+        databaseUtilsHelperRepository.deleteLinkCustomerAccount();
+        databaseUtilsHelperRepository.deleteAccountHistory();
+        databaseUtilsHelperRepository.deleteCustomer();
+        databaseUtilsHelperRepository.deleteAccount();
+        databaseUtilsHelperRepository.deleteAccountType();
+    }
+
+    public AccountType createAndSaveRandomAccountType() {
+        AccountType accountType = AccountType.builder()
+                .description("Test account type")
+                .reference(UUID.randomUUID().toString())
+                .build();
+        return accountTypeRepository.save(accountType);
+    }
+
+    public Account createAndSaveRandomAccount(AccountType accountType) {
+        Account account = Account.builder()
+                .description("Test account")
+                .accountTypeId(AggregateReference.to(accountType.getId()))
+                .build();
+        for (int i = 0; i < 3; i++) {
+            account.getAccountHistories().add(AccountHistory.builder()
+                    .description(UUID.randomUUID().toString())
+                    .timeStamp(OffsetDateTime.now())
+                    .build());
+        }
+        return accountRepository.save(account);
+    }
+
+    public Customer createAndSaveRandomCustomer() {
+        Customer customer = Customer.builder()
+                .firstName("Test")
+                .lastName("Customer")
+                .reference("TEST_CUSTOMER_" + UUID.randomUUID().toString())
+                .build();
+        return customerRepository.save(customer);
+    }
+
+    public long countLinkCustomerAccounts() {
+        return databaseUtilsHelperRepository.countLinkCustomerAccounts();
+    }
+
+    public void setAccountHistoryDescriptionNotNull() {
+        databaseUtilsHelperRepository.setAccountHistoryDescriptionNotNull();
+    }
+
+    public void dropAccountHistoryDescriptionNotNull() {
+        databaseUtilsHelperRepository.dropAccountHistoryDescriptionNotNull();
+    }
+
+    public long countAccountHistories() {
+        return databaseUtilsHelperRepository.countAccountHistories();
+    }
+
+}
