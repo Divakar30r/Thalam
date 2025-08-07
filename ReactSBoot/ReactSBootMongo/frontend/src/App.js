d6a8c17adcbc8e974665a3a772f85cc67cb82bca
Divakar30r <150545013+Divakar30r@users.noreply.github.com>
2025-08-08 02:31:05 +0530

    old commits sync (#9)
    * 'Keycloak'
    
    * Merge pull request #6 from Divakar30r/VSMain (#7) (#8)
    
    ---------
    
    Co-authored-by: divakar30r <divakar30r@gmail.com>

--- ReactSBoot/ReactSBootMongo/frontend/src/App.js
+++ ReactSBoot/ReactSBootMongo/frontend/src/App.js
@@ -1,46 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  
 import RecordForm from './components/RecordForm';
 import RecordList from './components/RecordList';
+import WelcomePage from './components/WelcomePage';
+import axios from 'axios';
 
 function App() {
-  const [records, setRecords] = useState([]);
 
-  useEffect(() => {
-    fetchRecords();
-  }, []);
-
-  const fetchRecords = async () => {
-    const response = await fetch('/api/records');
-    const data = await response.json();
-    setRecords(data);
+    const handleRecordSubmit = async (formData) => {
+    try {
+      const response = await axios.post('/api/records', formData);
+      console.log("Record created:", response.data);
+      // Optionally, you might want to refresh the record list or update state.
+    } catch (error) {
+      console.error("Error creating record:", error);
+    }
   };
-
-  const addRecord = async (record) => {
-   
-    await fetch('/api/records', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(record),
-    });
-    fetchRecords();
-  };
-
-  const deleteRecord = async (id) => {
-    await fetch(`/api/records/${id}`, {
-      method: 'DELETE',
-    });
-    fetchRecords();
-  };
- 
 
   return (
-    <div>
-      <h1>Record Management</h1>
-      <RecordForm onSubmit={addRecord}  />
-      <RecordList records={records} deleteRecord={deleteRecord} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/records" element={
+          <div style={{ margin: '20px' }}>
+            <h1>Record Management</h1>
+            <RecordForm onSubmit={handleRecordSubmit}/>
+            <RecordList />
+          </div>
+        }/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- ReactSBoot/ReactSBootMongo/frontend/src/index.js
+++ ReactSBoot/ReactSBootMongo/frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import fnkeycloak from './Appkeycloak';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+     <ReactKeycloakProvider authClient={fnkeycloak}   initOptions={{ onLoad: 'login-required' }}>
     <App />
+    </ReactKeycloakProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
